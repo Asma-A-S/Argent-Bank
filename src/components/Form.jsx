d3f0958a3653eebc3331661d853ed1a7abe6068d
asma benzerrouk <asma.benzerrouk@gmail.com>
2025-04-04 15:07:38 +0200

    formulaire de login avec email et mot de passe

---
+++ src/components/Form.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+export function Form({ onSubmit }) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        onSubmit({ email, password })
+    }
+    return (
+        <section className="sign-in-content">
+            <i className="fa fa-user-circle sign-in-icon"></i>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="input-wrapper">
+                    <label htmlFor="email">Username</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div className="input-remember">
+                    <input type="checkbox" id="remember-me" />
+                    <label htmlFor="remember-me">Remember me</label>
+                </div>
+
+                <button className="sign-in-button">Sign In</button>
+            </form>
+        </section>
+    )
+}
